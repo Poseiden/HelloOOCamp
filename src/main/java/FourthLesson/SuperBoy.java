7cb791a6d42da6de0fa833b361d2f65f0f265630
HaiboZhu <hbzhu@thoughtworks.com>
2016-12-01 13:46:35 +0800

    [hbzhu] refactor sort method in super boy.

--- src/main/java/FourthLesson/SuperBoy.java
+++ src/main/java/FourthLesson/SuperBoy.java
@@ -3,11 +3,8 @@ package FourthLesson;
 import FirstLesson.Car;
 import FirstLesson.ParkingLot;
 import ThirdLesson.Boy;
-import ThirdLesson.SmartBoy;
-import com.google.common.collect.Lists;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class SuperBoy extends Boy{
@@ -16,9 +13,7 @@ public class SuperBoy extends Boy{
     }
 
     public String park(Car car) {
-        Collections.sort(parkingLots, new Comparator<ParkingLot>() {
-            @Override
-            public int compare(ParkingLot o1, ParkingLot o2) {
+        Collections.sort(parkingLots, (o1, o2) -> {
                 double result = o2.getEmptyRate() - o1.getEmptyRate();
                 if(result > 0) {
                     return 1;
@@ -28,7 +23,7 @@ public class SuperBoy extends Boy{
                     return -1;
                 }
             }
-        });
+        );
         return parkingLots.get(0).park(car);
     }
 }
